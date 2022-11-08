309f1834fe969bb89470fa623348c402ea702018
Fulbabu <fulbabucse@gmail.com>
2022-11-08 14:25:37 +0600

    my reviews complete

--- src/pages/MyReviews/MyReviews.jsx
+++ src/pages/MyReviews/MyReviews.jsx
@@ -1,9 +1,164 @@
 import React from "react";
+import { useState } from "react";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { AuthContexts } from "../../contexts/AuthProvider/AuthProvider";
+import { FaEdit, FaTimes } from "react-icons/fa";
 
 const MyReviews = () => {
+  const [reviews, setReviews] = useState([]);
+  const { user } = useContext(AuthContexts);
+  useEffect(() => {
+    fetch(`http://localhost:5000/customerReview?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => setReviews(data))
+      .catch((err) => console.error(err));
+  }, []);
+  console.log(reviews[0]);
   return (
-    <div>
-      <h1>My Reviews</h1>
+    <div className="max-w-screen-xl mx-auto mt-3">
+      <h1 className="text-center text-2xl font-bold text-gray-700">
+        My Reviews
+      </h1>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full">
+                <thead className="bg-white border-b">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
+                    >
+                      No
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
+                    >
+                      Service Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
+                    >
+                      Price
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
+                    >
+                      Rating
+                    </th>
+
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
+                    >
+                      Review
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 py-4 text-left"
+                    >
+                      Service Image
+                    </th>
+                    <th
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-2 py-4 text-left"
+                    >
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {reviews.map((review, index) => (
+                    <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                      <td className="px-2 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {index + 1}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-2 py-4 whitespace-nowrap">
+                        {review?.serviceName}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-2 py-4 whitespace-nowrap">
+                        ${review?.price}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light px-2 py-4 whitespace-nowrap">
+                        {review?.rating}
+                      </td>
+                      <td className="text-sm capitalize text-gray-900 font-light px-2 py-4 whitespace-nowrap">
+                        {review?.review.slice(0, 50)}
+                      </td>
+                      <td className="text-sm text-gray-900 font-light py-2 whitespace-nowrap">
+                        <img
+                          className="w-12 h-12 rounded-md"
+                          src={review?.image}
+                          alt={review?.serviceName}
+                        />
+                      </td>
+                      <td className="flex gap-3 mt-3">
+                        <Link>
+                          <button
+                            type="submit"
+                            className="
+      w-full
+      px-6
+      py-2.5
+      bg-purple-600
+      text-white
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      rounded
+      shadow-md
+      hover:bg-purple-700 hover:shadow-lg
+      focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
+      active:bg-purple-800 active:shadow-lg
+      transition
+      duration-150
+      ease-in-out"
+                          >
+                            <FaEdit></FaEdit>
+                          </button>
+                        </Link>
+
+                        <Link>
+                          <button
+                            type="submit"
+                            className="
+      w-full
+      px-6
+      py-2.5
+      bg-red-600
+      text-white
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      rounded
+      shadow-md
+      hover:bg-red-700 hover:shadow-lg
+      focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0
+      active:bg-red-800 active:shadow-lg
+      transition
+      duration-150
+      ease-in-out"
+                          >
+                            <FaTimes></FaTimes>
+                          </button>
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
