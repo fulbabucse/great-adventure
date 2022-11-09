d59f06f3fc1401706dec4f543e0f7f17bf479ae3
Fulbabu <fulbabucse@gmail.com>
2022-11-09 19:42:22 +0600

    services page spinners added

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -3,12 +3,21 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import Service from "../Shared/Service/Service";
+import { BounceLoader } from "react-spinners";
 
 const Services = () => {
   const [services, setServices] = useState([]);
   const [count, setCount] = useState(0);
   const [page, setPage] = useState(0);
   const [size, setSize] = useState(6);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
 
   useEffect(() => {
     fetch(`http://localhost:5000/services?page=${page}&size=${size}`)
@@ -32,45 +41,56 @@ const Services = () => {
       <Helmet>
         <title>Services - Great Adventure with Fahim</title>
       </Helmet>
-      <h3 className="text-3xl font-bold text-purple-600 text-center mb-3">
-        Here My All Services
-        <p className="feature-underline"></p>
-      </h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {services?.map((service) => (
-          <Service key={service._id} service={service}></Service>
-        ))}
-      </div>
-      <div className="flex justify-center mt-4">
-        <nav aria-label="Page navigation example">
-          <ul className="flex gap-2 items-center list-style-none">
-            <li className="page-item">
-              <button
-                onClick={() => handlePrevious()}
-                disabled={page === 0}
-                className="page-link relative block cursor-pointer py-1.5 px-3 border-0 bg-gray-200 outline-none transition-all duration-300 rounded-full text-gray-800 hover:text-gray-800 hover:bg-gray-300 focus:shadow-none"
-                tabIndex="-1"
-              >
-                Previous
-              </button>
-            </li>
-            {[...Array(pages).keys()].map((number) => (
-              <li key={number}>
-                <button
-                  onClick={() => setPage(number)}
-                  className={`${
-                    page === number && "bg-blue-400 text-white"
-                  } page-link cursor-pointer relative block py-1 px-3 border-0 outline-none transition-all duration-300 rounded hover:bg-blue-500 focus:shadow-none hover:text-white`}
-                >
-                  {number + 1}
-                  <span className="visually-hidden">(current)</span>
-                </button>
-              </li>
+      {loading ? (
+        <div className="flex justify-center min-h-screen mt-6 lg:mt-48">
+          <BounceLoader
+            color={"#36d7b7"}
+            loading={loading}
+            aria-label="Loading Spinner"
+            data-testId="loader"
+          />
+        </div>
+      ) : (
+        <>
+          <h3 className="text-3xl font-bold text-purple-600 text-center mb-3">
+            Here My All Services
+            <p className="feature-underline"></p>
+          </h3>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {services?.map((service) => (
+              <Service key={service._id} service={service}></Service>
             ))}
-            <li className="page-item">
-              <select
-                onChange={(e) => setSize(e.target.value)}
-                className="form-select appearance-none
+          </div>
+          <div className="flex justify-center mt-4">
+            <nav aria-label="Page navigation example">
+              <ul className="flex gap-2 items-center list-style-none">
+                <li className="page-item">
+                  <button
+                    onClick={() => handlePrevious()}
+                    disabled={page === 0}
+                    className="page-link relative block cursor-pointer py-1.5 px-3 border-0 bg-gray-200 outline-none transition-all duration-300 rounded-full text-gray-800 hover:text-gray-800 hover:bg-gray-300 focus:shadow-none"
+                    tabIndex="-1"
+                  >
+                    Previous
+                  </button>
+                </li>
+                {[...Array(pages).keys()].map((number) => (
+                  <li key={number}>
+                    <button
+                      onClick={() => setPage(number)}
+                      className={`${
+                        page === number && "bg-blue-400 text-white"
+                      } page-link cursor-pointer relative block py-1 px-3 border-0 outline-none transition-all duration-300 rounded hover:bg-blue-500 focus:shadow-none hover:text-white`}
+                    >
+                      {number + 1}
+                      <span className="visually-hidden">(current)</span>
+                    </button>
+                  </li>
+                ))}
+                <li className="page-item">
+                  <select
+                    onChange={(e) => setSize(e.target.value)}
+                    className="form-select appearance-none
       block
       w-14
       px-3
@@ -85,26 +105,28 @@ const Services = () => {
       ease-in-out
       m-0
       focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                aria-label="Default select example"
-              >
-                <option value="3">3</option>
-                <option selected value="6">
-                  6
-                </option>
-              </select>
-            </li>
-            <li className="page-item">
-              <button
-                onClick={() => handleNext()}
-                disabled={page + 2 > pages}
-                className="page-link relative block py-1.5 px-3 border-0 bg-gray-200 outline-none transition-all duration-300 rounded-full text-gray-800 hover:text-gray-800 hover:bg-gray-300 focus:shadow-none"
-              >
-                Next
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </div>
+                    aria-label="Default select example"
+                  >
+                    <option value="3">3</option>
+                    <option selected value="6">
+                      6
+                    </option>
+                  </select>
+                </li>
+                <li className="page-item">
+                  <button
+                    onClick={() => handleNext()}
+                    disabled={page + 2 > pages}
+                    className="page-link relative block py-1.5 px-3 border-0 bg-gray-200 outline-none transition-all duration-300 rounded-full text-gray-800 hover:text-gray-800 hover:bg-gray-300 focus:shadow-none"
+                  >
+                    Next
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </>
+      )}
     </div>
   );
 };
